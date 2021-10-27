b146ed8a818b7815fbefb85ac3b3eb5e067f5296
Irina686 <84530490+Irina686@users.noreply.github.com>
2021-10-27 11:35:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WrireLine("Внесла изменения");
+        }
+    }
+}
